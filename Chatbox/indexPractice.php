c9d11fcb1c23741142ca162fb7603bd8e712b985
unknown <sriramrameshbabu98@gmail.com>
2017-10-20 17:20:08 -0500

    practice PHP

---
+++ Chatbox/indexPractice.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Test Chat </title>
+</head>
+<body>
+<div id="wrapper">
+    <div id="menu">
+        <p class="Welcome To r/place!">Welcome, <b></b></p>
+        <p class="Logout"><a id="exit" href="#">Exit Chat</a></p>
+        <div style="clear:both"></div>
+    </div>
+     
+    <div id="chatbox"></div>
+     
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" size="63" />
+        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    </form>
+</div>
+
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+
+</body>
+</html>
